ca3bf1c271d2b9328bd8e340c78efdf4743645a9
RAJPRITA <111212449+RAJPRITA@users.noreply.github.com>
2022-10-23 10:42:12 +0530

    code updated from the last lacture

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,123 +1,53 @@
-"""
-================================
-Recognizing hand-written digits
-================================
-
-This example shows how scikit-learn can be used to recognize images of
-hand-written digits, from 0-9.
-
-"""
-
 # Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
 # License: BSD 3 clause
 
 
-#part: library dependencies --sklearn, torch, tensorflow, numpy, transformer
-# Standard scientific Python imports
-import matplotlib.pyplot as plt
+# PART: library dependencies -- sklear, torch, tensorflow, numpy, transformers
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
-from sklearn.model_selection import train_test_split
+from joblib import dump, load
+from utils import preprocess_digits, train_dev_test_split, h_param_tuning, data_viz, pred_image_viz, get_all_h_param_comb
 
-GAMMA = 0.001
-train_frac = 0.1
-test_frac = 0.1
-dev_frac = 0.1
 
-###############################################################################
-# Digits dataset
-# --------------
-#
-# The digits dataset consists of 8x8
-# pixel images of digits. The ``images`` attribute of the dataset stores
-# 8x8 arrays of grayscale values for each image. We will use these arrays to
-# visualize the first 4 images. The ``target`` attribute of the dataset stores
-# the digit each image represents and this is included in the title of the 4
-# plots below.
-#
-# Note: if we were working from image files (e.g., 'png' files), we would load
-# them using :func:`matplotlib.pyplot.imread`.
+train_frac, dev_frac, test_frac = 0.8, 0.1 , 0.1
+assert train_frac + dev_frac + test_frac == 1.
 
-#part: Load dataset -- data from csv,tsv,jsonl,pickle
+# 1. set the ranges of hyper parameters
+gamma_list = [0.01, 0.005, 0.001, 0.0005, 0.0001]
+c_list = [0.1, 0.2, 0.5, 0.7, 1, 2, 5, 7, 10]
 
+params = {}
+params['gamma'] = gamma_list
+params['C'] = c_list
+
+h_param_comb = get_all_h_param_comb(params)
+
+
+
+
+# PART: load dataset -- data from csv, tsv, jsonl, pickle
 digits = datasets.load_digits()
+data_viz(digits)
+data, label = preprocess_digits(digits)
+# housekeeping
+del digits
 
-#PART:Sanity check visualization of the data
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
+model_path =train_save_model(data,label,train_frac,dev_frac,None,h_param_combo)
 
-###############################################################################
-# Classification
-# --------------
-#
-# To apply a classifier on this data, we need to flatten the images, turning
-# each 2-D array of grayscale values from shape ``(8, 8)`` into shape
-# ``(64,)``. Subsequently, the entire dataset will be of shape
-# ``(n_samples, n_features)``, where ``n_samples`` is the number of images and
-# ``n_features`` is the total number of pixels in each image.
-#
-# We can then split the data into train and test subsets and fit a support
-# vector classifier on the train samples. The fitted classifier can
-# subsequently be used to predict the value of the digit for the samples
-# in the test subset.
-
-# flatten the images
-n_samples = len(digits.images)
-data = digits.images.reshape((n_samples, -1))
-
-X_train, X_dev_test, y_train, y_dev_test = train_test_split(
-    data, digits.target, test_size=1-train_frac, shuffle=True
-)
-
-X_test,x_dev, y_test,y_dev = train_test_split(
-    X_dev_test, y_dev_test, test_size=(dev_frac)/(1-train_frac), shuffle=True
-)
-
-
-# Create a classifier: a support vector classifier
-clf = svm.SVC()
-#PART:Set hyperparameter
-hyper_params ={'gama':GAMMA}
-clf.set_params(**hyper_params)
-
-# Split data into 50% train and 50% test subsets
-
-# Learn the digits on the train subset
-clf.fit(X_train, y_train)
-
+best_model = load("svm_" + best_param_config +".joblib")
+# PART: Get test set predictions
 # Predict the value of the digit on the test subset
-predicted = clf.predict(X_test)
+predicted = best_model.predict(x_test)
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
+pred_image_viz(x_test, predicted)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
-
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
-
+# 4. report the test set accurancy with that best model.
+# PART: Compute evaluation metrics
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
 
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
+print("Best hyperparameters were:")
+print(best_h_params)
