5fa7977c0695909f369afdd554859b6799f98b8d
RAJPRITA <111212449+RAJPRITA@users.noreply.github.com>
2022-11-20 18:32:10 +0530

    clone from  the professor repository

--- api/app.py
+++ api/app.py
@@ -1,35 +1,33 @@
 from flask import Flask
-from flask import jsonify
 from flask import request
 from joblib import load
 
 app = Flask(__name__)
-model_path = "svm_gamma=0.001_C=0.5.joblib"
+model_path = "svm_gamma=0.0005_C=2.joblib"
 model = load(model_path)
-
 
 @app.route("/")
 def hello_world():
-    return "<b><p>Hello, World!</p><b>"
+    return "<!-- hello --> <b> Hello, World!</b>"
 
 
-@app.route("/sum", methods=["POST"])
+# get x and y somehow    
+#     - query parameter
+#     - get call / methods
+#     - post call / methods ** 
+
+@app.route("/sum", methods=['POST'])
 def sum():
     x = request.json['x']
     y = request.json['y']
-    z = x + y
-    return {'sum': z}
+    z = x + y 
+    return {'sum':z}
+
 
 
 @app.route("/predict", methods=['POST'])
 def predict_digit():
-    image1 = request.json['image1']
-    image2 = request.json['image2']
+    image = request.json['image']
     print("done loading")
-    predicted1 = model.predict([image1])
-    predicted2 = model.predict([image2])
-    if int(predicted1[0]) == int(predicted2[0]):
-        val = 'Same number'
-    else:
-        val = 'Different numbers'
-    return {"prediction": val}
+    predicted = model.predict([image])
+    return {"y_predicted":int(predicted[0])}
